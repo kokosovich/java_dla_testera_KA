4decd0f3903c7c3e23ec8d7eb69e91d6774e0ba2
Konrad Alfawicki <konrad.alfawicki@s3group.com>
2021-05-18 15:30:10 +0200

    Updated MainApp

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,9 +1,31 @@
 public class MainApp {
 
     public static void main(String[] args) {
-        // start aplikacji
 
-        System.out.println("Hello World");
+        // liczby całkowite
+        int number = 10; //deklaracja i inicjalizacja
+        System.out.println(number);
+
+        // liczby zmiennoprzecinkowe
+        double doubleNumber = 10.5;
+        System.out.println(doubleNumber);
+
+        // text
+        String someText = "text w javie";
+        System.out.println(someText);
+//        System.out.println("text w javie");
+
+        // pojedynczy znak
+        char someChar = 'a';
+        System.out.println(someChar);
+
+        // prawda
+        boolean trueValue = true;
+        System.out.println(trueValue);
+
+        // fałsz
+        boolean falseValue = false;
+        System.out.println(falseValue);
 
     }
 

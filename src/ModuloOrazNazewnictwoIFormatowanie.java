3c6e327c50b9834938f2f616ef1cee6b6df48d27
Konrad Alfawicki <konrad.alfawicki@s3group.com>
2021-05-20 09:43:21 +0200

    Added ZadanieDomowe1

---
+++ src/ModuloOrazNazewnictwoIFormatowanie.java
@@ -0,0 +1,31 @@
+public class ModuloOrazNazewnictwoIFormatowanie {
+
+    public static void main(String[] args) {
+
+        // Nazwy klas
+        // User, SuperUser, AdminUser
+
+        // Zmienne
+        String superUser = "Jestem super user";
+        int numberTen = 10;
+        boolean isTrue = true;
+
+        // formatowanie kodu - Ctrl+Alt+L
+        if (10 > 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        System.out.println(10 % 5);
+        System.out.println(10 % 3);
+        System.out.println(10 % 4);
+
+        if (9 % 2 == 0) {
+            System.out.println("liczba parzysta");
+        } else {
+            System.out.println("liczba nieparzysta");
+        }
+
+    }
+}
--- src/ZadanieDomowe1.java
+++ src/ZadanieDomowe1.java
@@ -2,7 +2,27 @@ public class ZadanieDomowe1 {
 
     public static void main(String[] args) {
 
-        System.out.println("Jestem w zadaniu domowym");
+        // 1. 4 zmienne typu int
+        // 2. Sprawdzić czy suma dwóch pierwszych jest parzysta
+        // 3. Sprawdzić czy suma dwóch pozostałych jest parzysta
+        // 4. Jeżeli dwie sumy są parzyste to wyświetl informacje, że wszystkie liczby są parzyste
+
+        int numOne = 7835;
+        int numTwo = 9323;
+        int numThree = 4291;
+        int numFour = 8573;
+
+        int sumOneTwo = numOne + numTwo;
+        int sumThreeFour = numThree + numFour;
+
+        boolean isSumOneTwoEven = (sumOneTwo % 2 == 0);
+        boolean isSumThreeFourEven = (sumThreeFour % 2 == 0);
+
+        if ((isSumOneTwoEven == true) && (isSumThreeFourEven == true)) {
+            System.out.println("sumy są parzyste");
+        } else {
+            System.out.println("sumy są nieparzyste");
+        }
 
     }
 }

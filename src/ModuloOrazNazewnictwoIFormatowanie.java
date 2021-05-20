ff75aa87da64d3780f4e228c8812d37e873fa6c1
Konrad Alfawicki <konrad.alfawicki@s3group.com>
2021-05-20 09:28:19 +0200

    Added ModuloAndStuff and ZadanieDomowe1

---
+++ src/ModuloOrazNazewnictwoIFormatowanie.java
@@ -0,0 +1,25 @@
+public class Modulo {
+
+    public static void main(String[] args) {
+
+        // Nazwy klas
+        // User, SuperUser, AdminUser
+
+        // Zmienne
+        String superUser = "Jestem super user";
+        int numberTen = 10;
+        boolean isTrue = true;
+
+        // formatowanie kodu - Ctrl+Alt+L
+        if (10 > 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        System.out.println(10 % 5);
+        System.out.println(10 % 3);
+        System.out.println(10 % 4);
+
+    }
+}

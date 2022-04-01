4e123a9de80eb08bf93103df1996651a93c1a4c7
Konrad Alfawicki <konrad.alfawicki@s3connectedhealth.com>
2022-04-01 19:09:15 +0200

    MainApp clean up

--- src/MainApp.java
+++ src/MainApp.java
@@ -10,20 +10,5 @@ public class MainApp {
         User tomek = new User("Tomek", "Smith", "tomek@test.pl", 30);
         tomek.getAllInfo();
 
-        System.out.println();
-
-        Bug defect = new Bug("Doesn't work", "joe.testowy@gmail.com", 2);
-        defect.showBugDetails();
-        defect.showReporter();
-        System.out.println("Bug priority: " + defect.getBugPriority());
-        defect.status = true;
-        defect.showBugStatus();
-
-        Bug issue = new Bug("It crashes", "joe.testowy@gmail.com", 5);
-        issue.showBugDetails();
-        issue.showReporter();
-        System.out.println("Bug priority: " + issue.getBugPriority());
-        issue.showBugStatus();
-
     }
 }

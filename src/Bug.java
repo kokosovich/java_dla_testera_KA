1276ac0a91383debb423ad97328873834ec1d999
kokos <kokosowy@gmail.com>
2022-04-01 19:04:42 +0200

    ZadanieDomowe2

---
+++ src/Bug.java
@@ -0,0 +1,43 @@
+public class Bug {
+    // POLA OBIEKTU
+    // - opis błędu
+    // - adres email osoby zgłaszającej błąd
+    // - priorytet błedu (1-5)
+    // - status błędu (otwarty/zamknięty) - domyślnie, podczas tworzenia zawsze otwarty
+
+    String description;
+    String reportersEmail;
+    int priority;
+    boolean status;
+
+    // KONSTRUKTOR
+
+    Bug(String description, String reportersEmail, int priority) {
+        this.description = description;
+        this.reportersEmail = reportersEmail;
+        this.priority = priority;
+        this.status = false;
+    }
+
+    //METODY
+    // - pokaż wszystkie informacje na temat błędu
+    void showBugDetails() {
+        System.out.println("Bug: " + description + " - reported by: " + reportersEmail + " - priority: " + priority + " - open: " + status);
+    }
+
+    // - pokaż osobę zgłaszającą błąd
+    void showReporter() {
+        System.out.println(("Bug reported by: " + reportersEmail));
+    }
+
+    // - pokaż status błędu
+    void showBugStatus() {
+        System.out.println("Bug status: " + " " + status);       
+    }
+
+    // - zwróć priorytet błędu
+    int getBugPriority() {
+        return priority;
+    }
+
+}
--- src/ZadanieDomowe2.java
+++ src/ZadanieDomowe2.java
@@ -1,23 +1,16 @@
-public class MainApp {
+public class ZadanieDomowe2 {
 
     public static void main(String[] args) {
-        // co to jest klasa i z czego się składa
-
-        // typ nazwa = nowy obiekt
-        User bartek = new User("Bartek", "Testowy", "bartek@test.pl", 17); // wywołanie konstruktora
-        bartek.getAllInfo();
-
-        User tomek = new User("Tomek", "Smith", "tomek@test.pl", 30);
-        tomek.getAllInfo();
-
-        System.out.println();
 
         Bug defect = new Bug("Doesn't work", "joe.testowy@gmail.com", 2);
         defect.showBugDetails();
         defect.showReporter();
+        defect.showBugStatus();
         System.out.println("Bug priority: " + defect.getBugPriority());
         defect.status = true;
         defect.showBugStatus();
+
+        System.out.println();
 
         Bug issue = new Bug("It crashes", "joe.testowy@gmail.com", 5);
         issue.showBugDetails();

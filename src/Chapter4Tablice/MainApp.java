24bdb5cfe81dc68af5291bc29b5e89aaf0e13c30
Konrad Alfawicki <konrad.alfawicki@s3connectedhealth.com>
2022-05-02 17:18:07 +0200

    Pętle for and for each

--- src/Chapter4Tablice/MainApp.java
+++ src/Chapter4Tablice/MainApp.java
@@ -4,33 +4,36 @@ public class MainApp {
 
     public static void main(String[] args) {
 
-    // 1. Tablice - zbiór danych tego samego typu
-    // 2. Zawsze musisz określić rozmiar tablicy
-    // 3. Ostatni element - długość tablicy -1
-    // 4. [] - tablica
-    // 5.  int[] numbers = new int[10]
+        // 1. Tablice - zbiór danych tego samego typu
+        // 2. Zawsze musisz określić rozmiar tablicy
+        // 3. Ostatni element - długość tablicy -1
+        // 4. [] - tablica
+        // 5.  int[] numbers = new int[10]
 
-    String[] names = new String[5];
+        String[] names = new String[5];
 
-    // wartości tablicy ["Bartek", "Marcin", "Asia", "Monika", "Tomek"]
-    // index tablicy    [   0,        1,        2,       3,        4  ]
+        // wartości tablicy ["Bartek", "Marcin", "Asia", "Monika", "Tomek"]
+        // index tablicy    [   0,        1,        2,       3,        4  ]
 
-    names[0] = "Bartek";
-    System.out.println(names[0]);
+        names[0] = "Bartek";
+        names[1] = "Marcin";
+        names[2] = "Asia";
+        names[3] = "Monika";
+        names[4] = "Tomek";
 
-    names[1] = "Marcin";
-    System.out.println(names[1]);
+        // for(licznik; warunek; zmiana licznika)
+
+        for(int i = 0; i < names.length; i++) {
+            if (i % 2 == 0) {
+                System.out.println(names[i]);
+            }
+        }
     
-    names[2] = "Asia";
-    System.out.println(names[2]);
-    
-    names[3] = "Monika";
-    System.out.println(names[3]);
+        // for each - zawsze przechodzi przez wszystkie elementy tablicy
 
-    names[4] = "Tomek";
-    System.out.println(names[4]);
+        for (String name : names) {
+            System.out.println(name);            
+        }
 
-    System.out.println(names[5]);
-    
     }
 }

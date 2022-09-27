b26b9d2b4063b6319457455d0e1599d70a401a23
Konrad Alfawicki <konrad.alfawicki@s3connectedhealth.com>
2022-09-27 17:45:52 +0200

    Zaczynamy moduł Interface

---
+++ src/Model/computer/Music.java
@@ -0,0 +1,36 @@
+package Model.computer;
+
+public interface Music {
+
+    // 1. NIE MOŻNA DODAWAĆ PÓL OBIEKTU
+    // 2. MOŻNA TWORZYĆ TYLKO STAŁE - DOMYŚLNIE public static final
+
+    String NAME = "MUSIC";
+
+    // 3. TYLKO METODY ABSTRAKCYJNE - DOMYŚLNIE public abstract
+
+    void playMusic();
+    void pauseMusic();
+    void stopMusic();
+
+    // OD JAVA 8
+    // 4. METODY DOMYŚLNE - ZWYKŁE METODY
+
+    default void sayHelloFromMusic() {
+        System.out.println("Hello from default");
+    }
+
+    // 5. METODY STATYCZNE
+    
+    static String getName() {
+        return NAME;
+    }
+
+    // OD JAVY 9
+    // 6. PRYWATNE STATYCZNE METODY
+
+    private static void privateMethod() {
+        System.out.println("Hello from private method");
+    }
+    
+}
---
+++ src/Module9Interface/MainApp.java
@@ -0,0 +1,5 @@
+package Module9Interface;
+
+public class MainApp {
+    
+}

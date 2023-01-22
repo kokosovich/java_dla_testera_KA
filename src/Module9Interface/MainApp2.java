cf3caa19bb8a08ba87b9695a29706eb88d8e3757
kokos <kokosowy@gmail.com>
2023-01-22 06:10:44 +0100

    Boxing

---
+++ src/Module9Interface/MainApp2.java
@@ -0,0 +1,44 @@
+package Module9Interface;
+
+public class MainApp2 {
+    
+    public static void main(String[] args) {
+        
+        // TYPY OPAKOWUJĄCE
+
+        Byte b;
+        Short s;
+        Integer i;
+        Long l;
+        Float f;
+        Double d;
+        Character c;
+        Boolean bl;
+
+        int number = 10;
+
+        // TEGO PONIŻEJ JUŻ SIĘ RACZEJ NIE UŻYWA
+        // Integer intNumber = new Integer(10);
+
+        // ZAMIAST TEGO LEPIEJ UŻYĆ
+        // AUTO BOXING
+        Integer intNumber = Integer.valueOf(10);
+        Integer intNumber2 = 10;
+
+        System.out.println(intNumber);
+        System.out.println(intNumber2);
+
+        Double dbl = new Double(3.10);
+
+        // AUTO UNBOXING
+        int newNumber = intNumber.intValue();
+
+        // ZAPAMIĘTAJ - CZASEM ZAMIAST TYPU PROSTEGO, 
+        // NIEZBĘDNE JEST UŻYCIE TYPU REFERENCYJNEGO
+        // NP. W KOLEKCJACH
+
+        List<int> numbers = new ArrayList<>();
+        
+    }
+
+}
